9b11b677d0999fe5bb0c2fa696d8d14d54b3fc10
gaojiucheng <gaojiucheng@angelalign.com>
2018-04-26 18:41:35 +0800

    constant常量类

---
+++ src/main/java/com/gaojc/gaming/bean/Constant.java
@@ -0,0 +1,14 @@
+package com.gaojc.gaming.bean;
+
+/**
+ * @author gaojc
+ * @date 2018/4/25
+ * @description
+ */
+public class Constant {
+    //回复数字
+    public static final String REAPLY_NUM_ONE = "1";
+    public static final String REAPLY_NUM_TWO = "2";
+    public static final String REAPLY_NUM_THREE = "3";
+    public static final String REAPLY_NUM_FOUR = "4";
+}
